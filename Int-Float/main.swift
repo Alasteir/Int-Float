b421f7f1149ea0b5cad66592394fa096c3ea2a54
Alasteir <76561392+Alasteir@users.noreply.github.com>
2024-03-15 13:35:11 +0200

    Initial commit

---
+++ Int-Float/main.swift
@@ -0,0 +1,10 @@
+print("Int: \(Int.min) to \(Int.max)")
+
+print("Float: \(Float.leastNormalMagnitude) to \(Float.greatestFiniteMagnitude)")
+
+print("Double: \(Double.leastNormalMagnitude) to \(Double.greatestFiniteMagnitude)")
+
+print("UInt: \(UInt.min) to \(UInt.max)")
+
+print("Int8: \(Int8.min) to \(Int8.max)")
+
